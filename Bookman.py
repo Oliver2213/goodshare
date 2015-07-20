302cfd66b5f93b03fca4d8c1eacaa5db51058384
oliver2213 <oliver22213@me.com>
2015-07-20 19:35:46 -0400

    initial commit

---
+++ Bookman.py
@@ -0,0 +1,7 @@
+# Bookman, the bookshare / Goodreads cross platform thing
+# Author Blake Oliver <oliver22213@me.com>
+#import our APIs
+from Goodreads-API import GoodreadsAPI, InvalidResponse
+import Bookshare-API
+# Import our UI stuff
+import wx
