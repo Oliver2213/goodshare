0c81692539be47590e4d83adb5f4cb9ac29374b4
oliver2213 <oliver22213@me.com>
2015-07-20 20:08:27 -0400

    Rearanged working dir, added bookshare api wrapper as a submod, deleted old bookman file

--- Bookman.py
+++
@@ -1,7 +0,0 @@
-# Bookman, the bookshare / Goodreads cross platform thing
-# Author Blake Oliver <oliver22213@me.com>
-#import our APIs
-from Goodreads-API import GoodreadsAPI, InvalidResponse
-import Bookshare-API
-# Import our UI stuff
-import wx
