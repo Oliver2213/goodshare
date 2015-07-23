3afef8210a916b98faebf01bc8be05c9565ce4af
oliver2213 <oliver22213@me.com>
2015-07-23 08:42:03 -0400

    Updated init code with some checks for future key and config key files

--- __init__.py
+++ __init__.py
@@ -1,5 +1,26 @@
 # Goodshare initialization stuff
 from goodreads import client
-from apikeys import *
-import secureconfig
+import sys
+#Set up configuration stuff.
+from secureconfig import SecureConfigParser
+confkey = 'goodshare.key'
+conffile = 'goodshare.conf'
+#Checking if we have the key to open up our configuration file and extract sensitive values
+if os.path.isfile(confkey)==False:
+  #A key file isn't in current directory
+  print "Error! No keyfile found in current directory! This means you won't be able to decrypt needed authorization information to access web services."
+  sys.exit()
+
+#Enstantiate a config parser object with our key
+config = SecureConfigParser.from_file(confkey)
+
+# Check if we've got a config file
+if os.path.isfile(conffile)==False:
+  # No configuration files in current directory.
+  print "Error! No configuration file found in current directory. Necessary API and access keys are not available. Exeting."
+  sys.exit()
+else # Since it exists, load it
+  config.load(conffile)
+
+
 gc = client.GoodreadsClient(gkey, gsecret)
