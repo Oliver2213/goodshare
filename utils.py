e722ed3f42bf4f584229a4df65c8be8841a8ca8a
oliver2213 <oliver22213@me.com>
2015-07-30 00:11:34 -0400

    Moved the authorize function to utils

--- utils.py
+++ utils.py
@@ -11,3 +11,22 @@ def saveconfig(filename, configclass):
   fh = open(filename, 'w')
   configclass.write(filename)
   fh.close()
+
+def authorize():
+  utils.debug("Passing auth request to goodreads wrapper")
+  gc.authenticate() #Pass the request to authorize off to the goodreads wrapper
+  #Convert the returned token strings from unicode to ascii
+  if gc.session.access_token and gc.session.access_token_secret:
+    utils.debug("Token values were returned.")
+  gc.session.access_token = gc.session.access_token.encode('ascii','ignore')
+  gc.session.access_token_secret = gc.session.access_token_secret.encode('ascii','ignore')
+  # Encrypt and save the resulting tokens in our config file:
+  config.set('APIKeys', 'g_token', gc.session.access_token, encrypt=True)
+  config.set('APIKeys', 'g_token_secret', gc.session.access_token_secret, encrypt=True)
+  if config.get('APIKeys', 'g_token') and config.get('APIKeys', 'g_token_secret'): # If values exist in our keyfile
+    if config.get('APIKeys', 'g_token') == gc.session.access_token and config.get('APIKeys', 'g_token_secret') == gc.session.access_token_secret: # if those values match our tokens
+      print "Retrieved and saved you're Goodreads access tokens!"
+    else:
+      print "Error, tokens saved in config do not match those that were just saved!"
+  else:
+    print "Error in retrieving Goodreads access tokens, please try again."
