e99be46c112e2bf9cedcc7a8c29fdf9848cabb06
oliver2213 <oliver22213@me.com>
2015-07-30 00:00:32 -0400

    Fixed a few syntax errors in main file

--- goodshare.py
+++ goodshare.py
@@ -46,7 +46,7 @@ if config.get('APIKeys', 'gkey') and config.get('APIKeys', 'gsecret'):
   utils.debug("Initializing goodreads class with provided API keys...")
   gc = client.GoodreadsClient(config.get('APIKeys', 'gkey'), config.get('APIKeys', 'gsecret')) # initialize our goodreads class with app tokens
 else:
-  print "Error! Necessary API keys are not present in configuration file. Goodshare can't authenticate to Goodreads.")
+  print "Error! Necessary API keys are not present in configuration file. Goodshare can't authenticate to Goodreads."
   sys.exit()
 
 if gc:
@@ -60,15 +60,15 @@ if config.get('APIKeys', 'g_token') and config.get('APIKeys', 'g_token_secret'):
   print "Authenticating to Goodreads."
   gc.authenticate(config.get('APIKeys', 'g_token'), config.get('APIKeys', 'g_token_secret'))
 else: # we don't have app spesific tokens, we need to authorize with Goodreads to get them
-  print "No user spesific goodreads access tokens available.")
+  print "No user spesific goodreads access tokens available."
   result = raw_input("Do you want to obtain them? This should only have to be done once, and is required to use goodreads with Goodshare. (y/n)")
-    if result == 'y' or result == 'yes':
-      authorize()
+  if result == 'y' or result == 'yes':
+    authorize()
 print "Logged into Goodreads as: %s!" %(gc.auth_user())
 
 #Functions
 
-def authorize()
+def authorize():
   utils.debug("Passing auth request to goodreads wrapper")
   gc.authenticate() #Pass the request to authorize off to the goodreads wrapper
   #Convert the returned token strings from unicode to ascii
@@ -83,6 +83,6 @@ def authorize()
     if config.get('APIKeys', 'g_token') == gc.session.access_token and config.get('APIKeys', 'g_token_secret') == gc.session.access_token_secret: # if those values match our tokens
       print "Retrieved and saved you're Goodreads access tokens!"
     else:
-      print 
+      print "Error, tokens saved in config do not match those that were just saved!"
   else:
     print "Error in retrieving Goodreads access tokens, please try again."
